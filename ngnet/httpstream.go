7d8d14b22054e787766719123781e1a555d8caa9
krishpranav <krisna.pranav@gmail.com>
2021-05-26 17:13:11 +0530

    [+] added init func [+]

--- ngnet/httpstream.go
+++ ngnet/httpstream.go
@@ -6,3 +6,8 @@ var (
 	httpRequestFirtLine  *regexp.Regexp
 	httpResponseFirtLine *regexp.Regexp
 )
+
+func init() {
+	httpRequestFirtLine = regexp.MustCompile(`([A-Z]+) (.+) (HTTP/.+)\r\n`)
+	httpResponseFirtLine = regexp.MustCompile(`(HTTP/.+) (\d{3}) (.+)\r\n`)
+}
